80a599e419b9c8319b85458b4485ce9f60b2cf46
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2023-01-28 15:43:33 +0200

    [DB URL] Split db url into different params

--- src/main/java/com/mlgmag/sql/db_management_system/constants/ConfigNames.java
+++ src/main/java/com/mlgmag/sql/db_management_system/constants/ConfigNames.java
@@ -5,9 +5,11 @@ public final class ConfigNames {
     private ConfigNames() {
     }
 
-    public static final String DB_URL_CONFIG = "DB_URL";
+    public static final String DB_URL_SCHEMA_CONFIG = "DB_URL_SCHEMA";
+    public static final String DB_HOST_CONFIG = "DB_HOST";
+    public static final String DB_PORT_CONFIG = "DB_PORT";
+    public static final String DB_NAME_CONFIG = "DB_NAME";
     public static final String DB_USERNAME_CONFIG = "DB_USERNAME";
     public static final String DB_PASSWORD_CONFIG = "DB_PASSWORD";
-    public static final String DB_NAME_CONFIG = "DB_NAME";
 
 }
